0ae425792685d1159628acff433e34917030fe62
Mohit Mayank <mohit_mayank@intuit.com>
2017-02-06 14:26:14 +0530

    Added and edited some preprocessing logic

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,36 @@
 import os
 import re
 from nltk.corpus import stopwords
-  
-if not os.path.exists('Cleaned scripts'):
-	os.mkdir('Cleaned scripts')
+
+clean_dir = 'scripts/clean/'
+
+if not os.path.exists(clean_dir):
+	os.mkdir(clean_dir)
+
 for root, directory, files in os.walk('./scripts'):
-	for file in files:
-		#print(root, directory, file)
-		file_path = os.path.join(root, file)
-		f = open(file_path, encoding="utf8")
-		text = f.read()
-		text = re.sub(r'\d\d\:\d\d\n', '', text)
-		text = re.sub(r'\n', ' ', text)
-		text = re.sub('\s+', ' ', text)
+	if '.DS_Store' in files:
+		files.remove('.DS_Store')
+	if root.split(os.sep)[-2] != 'clean':
+		for file in files:
+			file_path = os.path.join(root, file)
+			print('Cleaning:', file_path.split(os.sep, maxsplit=2)[-1])
+			f = open(file_path, encoding='utf8')
+			text = f.read()
+			f.close()
+			text = text.lower()
+			text = re.sub(r'\d\d\:\d\d\n', '', text)
+			text = re.sub('[#.,$%|~\-\/&\"\'`*+=!?;:()^]', '', text)
+			text = re.sub(r'\n', ' ', text)
+			text = re.sub('\s+', ' ', text)
 
-		words = text.split()
-		filtered_words = [word for word in words if word not in stopwords.words('english')]
-		text = ' '.join(filtered_words)
+			words = text.split()
+			filtered_words = [word for word in words if word not in stopwords.words('english')]
+			text = ' '.join(filtered_words)
 
-		dir_ = root.split('\\')[1]
-		if not os.path.exists('Cleaned scripts\\'+dir_):
-			os.makedirs('Cleaned scripts\\'+dir_)
-		file = os.path.join(os.getcwd()+'\\Cleaned scripts\\'+dir_, file)
-		fout = open(file, 'w', encoding="utf8")
-		fout.write(text)
+			dir_ = root.split(os.sep)[2]
+			if not os.path.exists(clean_dir + dir_):
+				os.makedirs(clean_dir + dir_)
+			file = os.path.join(os.getcwd(), clean_dir, dir_, file)
+			fout = open(file, 'w', encoding='utf-8')
+			fout.write(text)
+			fout.close()
