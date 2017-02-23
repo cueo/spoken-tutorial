30f0ec2b34a6b97993a5b0732bdf72b3012d2319
cueo <mohit.ritanil@gmail.com>
2017-02-23 10:57:08 +0530

    Removed future imports

--- lda.py
+++ lda.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import os
 from preprocess import clean
 from time import time
@@ -53,7 +52,7 @@ def create_dataset():
 # =============================================================================================
 
 
-def lda(path):
+def lda(data_samples):
 	n_samples = 2000
 	n_features = 1000
 	n_topics = 3
@@ -71,55 +70,55 @@ def lda(path):
 	# footers and quoted replies, and common English words, words occurring in
 	# only one document or in at least 95% of the documents are removed.
 
-	# print("Loading dataset...")
+	# print "Loading dataset..."
 	t0 = time()
 	# dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 	# data_samples = dataset.data[:n_samples]
 	dataset = create_dataset()
 	# data_samples = dataset
-	with open(path, 'r') as f:
-		data_samples = clean(f.read())
-		data_samples = [x[0] for x in data_samples]
-	# print("done in %0.3fs." % (time() - t0))
-	# print(dataset)
+	# print "done in %0.3fs." % (time() - t0)
+	# print dataset
 
 	# Use tf-idf features for NMF.
-	# print("Extracting tf-idf features for NMF...")
+	# print "Extracting tf-idf features for NMF..."
 	tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
 	t0 = time()
 	tfidf = tfidf_vectorizer.fit_transform(data_samples)
-	# print("done in %0.3fs." % (time() - t0))
+	# print "done in %0.3fs." % (time() - t0)
 
 	# Use tf (raw term count) features for LDA.
-	# print("Extracting tf features for LDA...")
+	# print "Extracting tf features for LDA..."
 	tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
 	t0 = time()
 	tf = tf_vectorizer.fit_transform(data_samples)
-	# print("done in %0.3fs." % (time() - t0))
+	# print "done in %0.3fs." % (time() - t0)
 
 	'''
 	# Fit the NMF model
-	print("Fitting the NMF model with tf-idf features, n_samples=%d and n_features=%d..." % (n_samples, n_features))
+	print "Fitting the NMF model with tf-idf features, n_samples=%d and n_features=%d..." % (n_samples, n_features)
 	t0 = time()
 	nmf = NMF(n_components=n_topics, random_state=1, alpha=.1, l1_ratio=.5).fit(tfidf)
-	print("done in %0.3fs." % (time() - t0))
+	print "done in %0.3fs." % (time() - t0)
 
-	print("\nTopics in NMF model:")
+	print "\nTopics in NMF model:"
 	tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-	print(get_top_words(nmf, tfidf_feature_names, n_top_words))
-	print()
+	print get_top_words(nmf, tfidf_feature_names, n_top_words)
+	print 
 	'''
 
-	# print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..." % (n_samples, n_features))
+	# print "Fitting LDA models with tf features, n_samples=%d and n_features=%d..." % (n_samples, n_features)
 	lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_method='online', learning_offset=50., random_state=0)
 	t0 = time()
 	lda.fit(tf)
-	# print("done in %0.3fs." % (time() - t0))
+	# print "done in %0.3fs." % (time() - t0)
 
-	# print("\nTopics in LDA model:")
+	# print "\nTopics in LDA model:"
 	tf_feature_names = tf_vectorizer.get_feature_names()
 	return get_top_words(lda, tf_feature_names, n_top_words)
 
 if __name__ == '__main__':
 	path = 'scripts/Advance C/Command-line-arguments-in-C.txt'
-	print(lda(path))
+	with open(path, 'r') as f:
+		data_samples = clean(f.read())
+		data_samples = [x[0] for x in data_samples]
+	print lda(data_samples)
