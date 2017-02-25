0fea8653025673ecbaf0e9bc31ec09416c1fa31e
cueo <mohit.ritanil@gmail.com>
2017-02-26 01:10:39 +0530

    Retrieve and pickle data from StackOverflow

--- data_parser.py
+++ data_parser.py
@@ -1,12 +1,13 @@
 import csv
 import pickle
+from stack import get_answers
+import os
 
 
 def get_questions(path):
 	with open(path) as csvfile:
 		reader = csv.reader(csvfile)
-		# dictionary of StackExchange with key=question id and value=question text
-		qids = {}
+		# dictionary of StackExchange with key=question id and value=question + answer text
 		reader.next()
 		reader.next()
 		for row in reader:
@@ -18,14 +19,27 @@ def get_questions(path):
 					links = [link]
 				for l in links:
 					qid = l.split('/')[-2]
-					qids[qid] = l
+					if qid not in qids:
+						try:
+							print 'Fetching:', qid
+							question, answers = get_answers(qid)
+							qids[qid] = [[question] + answers]
+						except Exception as e:
+							print "Couldn't fetch, error:", e
+					else:
+						print 'Already fetched question:', qid
 	return qids
 
 
 if __name__ == '__main__':
 	path = 'data/Links to topics v2.1 - Sheet1.csv'
-	qids = get_questions(path)
+	pickle_path = 'data/data.pkl'
+	qids = {}
+	if os.path.exists(pickle_path):
+		with open(pickle_path, 'r') as f:
+			qids = pickle.load(f)
+	get_questions(path)
 	# for k, v in qids.items():
 	# 	print k, v
-	with open('data.pkl', 'w') as f:
+	with open(pickle_path, 'w') as f:
 		pickle.dump(qids, f)
