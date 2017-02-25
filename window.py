72b4f7b86268b78cbb99c283b4a36f8ed5a9bcc4
cueo <mohit.ritanil@gmail.com>
2017-02-26 01:11:55 +0530

    Load forum data from pickle if available instead of fetching online

--- window.py
+++ window.py
@@ -1,3 +1,6 @@
+import pickle
+import os
+
 from preprocess import clean
 from datetime import datetime
 from stack import get_answers
@@ -7,15 +10,15 @@ from similarity import calculate_similarity
 interval = 60
 
 
-"""
-Given a transcript file, create snippets of time interval = interval.
-script = each line in the transcript (broken at the subtitle timing breaks)
-times = timestamps in the video corresponding to each of the lines in the script
-time_interval_index = a list where each element, i, is the index of the list times
-	such that scripts[i] and scripts[times[i]] are interval seconds apart
-	therefore, the window = snippet between scripts[i] and scripts[times[i]]
-"""
 def calculate_interval(path):
+	"""
+	Given a transcript file, create snippets of time interval = interval.
+	script = each line in the transcript (broken at the subtitle timing breaks)
+	times = timestamps in the video corresponding to each of the lines in the script
+	time_interval_index = a list where each element, i, is the index of the list times
+		such that scripts[i] and scripts[times[i]] are interval seconds apart
+		therefore, the window = snippet between scripts[i] and scripts[times[i]]
+	"""
 	with open(path, 'r') as f:
 		text = clean(f.read())
 	script = [x[0] for x in text]
@@ -51,31 +54,46 @@ if __name__ == '__main__':
 	"""
 	Compare snippet against forum text
 	"""
-	ids = ['2377466', '333889', '571076', '3661251']
+	# first two ids are irrelevant, last two are relevant
+	ids = ['330793', '333889', '571076', '3661251']
+	qids = []
+	path = 'data/data.pkl'
+	if os.path.exists(path):
+		with open(path, 'r') as f:
+			stack_data = pickle.load(f)
+			qids = stack_data.keys()
 	forum_texts = []
 	for id_ in ids:
-		print 'Fetching:', id_
-		question, answers = get_answers(id_)
-		"""
-		Model for vanilla comparison
-		"""
-		# forum_texts.append(question + '\n' + ' '.join(answers))
-		"""
-		Bag of post model
-		"""
-		forum_texts.append([question] + answers)
+		if id_ in qids:
+			forum_texts.append(stack_data[id_])
+		else:
+			print 'Fetching:', id_
+			question, answers = get_answers(id_)
+			"""
+			Model for vanilla comparison
+			"""
+			# forum_texts.append(question + '\n' + ' '.join(answers))
+			"""
+			Bag of post model
+			"""
+			forum_texts.append([question] + answers)
 	"""
 	Pairwise similarity without learning weight of upvotes
 	"""
 	similarity = []
 	for text in forum_texts:
 		sim = 0
+		l = 0
 		for post in text:
 			l = len(post)
 			sim += calculate_similarity([snippets[73], post])[1]
 		sim /= (l * 1.0)
 		similarity.append(sim)
-
 	print 'Snippet:', snippets[73]
 	print 'Forum text:', forum_texts
 	print similarity
+
+	"""
+	Compare data against relevant and irrelevant forums.
+	"""
+
