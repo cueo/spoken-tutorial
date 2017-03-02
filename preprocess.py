13a51dd6d0876ccaa0f7186e125060e5db2a2c94
cueo <mohit.ritanil@gmail.com>
2017-03-02 20:53:28 +0530

    Lot of changes, fixed some bugs

--- preprocess.py
+++ preprocess.py
@@ -20,56 +20,48 @@ def clean(text):
 	text = text.lower()
 	text = re.sub('[#.,$%|~\-/&\"\'`*+=!?;(){}^]', '', text)
 	text = re.sub('\n ', '\n', text)
-	text = re.sub('\n\n+', '\n\n', text)
+	text = re.sub('\n+', '\n', text)
 	text = re.sub(' +', ' ', text)
-
-	lines = text.split('\n\n')
-	found = False
-	for index, line in enumerate(lines):
-		if 'end of this tutorial' in line:
-			found = True
-			break
-	if found:
-		lines = lines[:index]
+	# text = re.sub(r'(\d+:\d+)[ \n]+(((?!\d+:\d+).)*)', r'\1\n\2\n', text, re.S)
+	matches = re.findall(r'(\d+:\d+)[ \n]+(((?!\d+:\d+).)*)', text, re.S)
 
 	cleaned_lines = []
-	for line in lines:
-		details = line.split('\n')
-		timestamp, words = details[0], details[1]
-		words = words.split(' ')
+	for match in matches:
+		timestamp = match[0]
+		line = re.sub('\n+', ' ', match[1])
+
+		if 'end of this tutorial' in line:
+			break
+
+		line = line.strip()
+		line = re.sub(r':([ ]?)', r'\1', line)
+		words = line.split(' ')
 		filtered_words = [word for word in words if word not in stop_words]
 		cleaned_lines.append((' '.join(filtered_words), timestamp))
 
 	return cleaned_lines
 
 
-def clean_all_scripts():
-	clean_dir = 'data/clean/'
-	if not os.path.exists(clean_dir):
-		os.mkdir(clean_dir)
-
-	for root, directory, files in os.walk('./scripts'):
-		if '.DS_Store' in files:
-			files.remove('.DS_Store')
-		if 'clean' not in root.split(os.sep):
-			for file in files:
-				file_path = os.path.join(root, file)
-				print 'Cleaning:', file_path.split(os.sep, maxsplit=2)[-1]
-				f = open(file_path, encoding='utf8')
-				text = f.read()
-				f.close()
-				text = clean(text)
-
-				dir_ = root.split(os.sep)[1]
-				if not os.path.exists(clean_dir + dir_):
-					os.makedirs(clean_dir + dir_)
-				file = os.path.join(os.getcwd(), clean_dir, dir_, file)
-				fout = open(file, 'w', encoding='utf-8')
-				fout.write(text)
-				fout.close()
+def clean_all_scripts(path):
+	"""
+	Check if all files are getting cleaned.
+	Exception shouldn't be thrown for any of the file.
+	"""
+	for r, d, files in os.walk(path):
+		for f in files:
+			if f.endswith('.txt'):
+				print 'Cleaning file:', f
+				file_path = os.path.join(r, f)
+				with open(file_path, 'r') as fp:
+					try:
+						clean(fp.read())
+					except Exception as e:
+						print "Couldn't clean file:", f
+						print 'Reason:', e
 
 
 if __name__ == '__main__':
-	# clean_all_scripts()
-	with open('data/BASH/Introduction-to-BASH-Shell-Scripting.txt', 'r') as f:
+	# clean_all_scripts(path)
+	path = 'data'
+	with open('data/BASH/Advance-topics-in-a-function.txt', 'r') as f:
 		print clean(f.read())
