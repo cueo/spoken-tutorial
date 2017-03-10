45eb8056ad071bb6570cb6d1c5ac0e2c9a15cc93
cueo <mohit.ritanil@gmail.com>
2017-03-10 22:57:55 +0530

    Conform to PEP8 standards

--- lsa.py
+++ lsa.py
@@ -9,18 +9,20 @@ from stack import get_answers
 
 import itertools
 import re
-import scipy as sc
+
 
 def remove_duplicates(text):
 	text = re.sub('\d+'," ",text)
 	return ' '.join(set(text.split()))
 
+
 def tf_matrix(documents):
 	tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0)
-	tfidf_matrix =  tf.fit_transform(documents)
-	feature_names = tf.get_feature_names() 
+	tfidf_matrix = tf.fit_transform(documents)
+	# feature_names = tf.get_feature_names()
+	# print feature_names
 	return tfidf_matrix
-	#print feature_names
+
 
 def svd(matrix):
 	svd = TruncatedSVD(100)
@@ -28,28 +30,29 @@ def svd(matrix):
 	matrix = lsa.fit_transform(matrix)
 	return matrix
 
-def cosine(vector1,vector2):	
+
+def cosine(vector1, vector2):
 	return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
+
 
 def lsa_similarity(documents):
 	tfidf_matrix = tf_matrix(documents)
 	lsa = svd(tfidf_matrix)
-
 	# print lsa
 
 	values = list(itertools.combinations(lsa, 2))
 	sim = 0
-
 	for i in values:
-		sim = sim + cosine(i[0], i[1])
+		sim += cosine(i[0], i[1])
 
 	return sim
+
 
 if __name__ == "__main__":
 	documents = []
 	transcript_path = 'data/C and Cpp/Working-With-Structures.txt'
-	f = open(transcript_path)
-	file = f.read()
+	with open(transcript_path, 'r') as f:
+		file = f.read()
 	transcript = simple_clean(file)
 	transcript = re.sub('\d+'," ",transcript)
 	documents.append(transcript)
